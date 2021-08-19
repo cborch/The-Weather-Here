d5cc2c2995a744aa8b0fb244a6d772989b7b5227
Carter Borchetta <carterborch@me.com>
2021-08-19 19:31:39 -0400

    Added support for taking an envirment variable port or starting on port 3000

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const Datastore = require('nedb')
 const fetch = require('node-fetch')
 require('dotenv').config()
 
-
-app.listen(3000, () => console.log('listening at 3000'))
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Starting server on port ${port}`))
 
 app.use(express.static('public'))
 app.use(express.json({ limit: '1mb' }))
