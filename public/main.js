4b474714ee66eb6219f785eb7d6be7966ad344cf
Carter Borchetta <carterborch@me.com>
2021-08-19 21:23:48 -0400

    Made a change to error handling so that areas with no aq will still POST

--- public/main.js
+++ public/main.js
@@ -2,43 +2,38 @@
 
 console.log("called main.js")
 
-let lat, long;
+let lat, long, weather, aq;
 
 if ('geolocation' in navigator) {
     console.log('geolocation available');
     navigator.geolocation.getCurrentPosition(async position => {
+        
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+        document.getElementById('latitude').textContent = lat;
+        document.getElementById('longitude').textContent = long;
+        // CORS is disabled on Dark Sky, so we can't share the API's resources with client side code
+        // - This is in part becuase the API key is part of the request so for security reasons we need to make the request to Dark Sky from our server
+        // In this client side code, we will make a request to our server at a new endpoint that we will create that asks our server to make a call to Dark Sky 
+        const apiURL = `/weather/${lat},${long}`
+        const response = await fetch(apiURL)
+        const json = await response.json()
+        console.log('tried to get weather')
+        const weather = json['weather']['currently']
+        document.getElementById('summary').textContent = weather['summary']
+        document.getElementById('temperature').textContent = weather['temperature']
+        
         try {
             console.log('Entered try')
-            lat = position.coords.latitude;
-            long = position.coords.longitude;
-            document.getElementById('latitude').textContent = lat;
-            document.getElementById('longitude').textContent = long;
-            // CORS is disabled on Dark Sky, so we can't share the API's resources with client side code
-            // - This is in part becuase the API key is part of the request so for security reasons we need to make the request to Dark Sky from our server
-            // In this client side code, we will make a request to our server at a new endpoint that we will create that asks our server to make a call to Dark Sky 
-            const apiURL = `/weather/${lat},${long}`
-            const response = await fetch(apiURL)
-            const json = await response.json()
-            console.log('tried to get weather')
-            const weather = json['weather']['currently']
             const aq = json['airQuality']['results'][2]
-            console.log('tried to get air qaulity')
-            document.getElementById('summary').textContent = weather['summary']
-            document.getElementById('temperature').textContent = weather['temperature']
             document.getElementById('aqParameter').textContent = aq['parameter']
             document.getElementById('aqValue').textContent = aq['value']
             document.getElementById('aqUnits').textContent = aq['unit']
             const aqDate = new Date(aq['date']['local'])
             document.getElementById('aqDate').textContent = aqDate.toDateString()
-            console.log('about to try to post data')
-
-
-    
-
-
         } catch (error) {
             console.log('Something went wrong')
-            aq = 'No air quality'
+            aq = -1
         }
 
         // Send to database 
@@ -55,6 +50,8 @@ if ('geolocation' in navigator) {
         console.log('tried to post data')
         console.log(dbJSON);
         console.log('tried to post')
+
+        
 
     });
 } else {
