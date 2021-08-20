3097dd5047d64bd171f9a49cc70c2fc98df162a6
Carter Borchetta <carterborch@me.com>
2021-08-19 20:50:50 -0400

    Debugging

--- public/main.js
+++ public/main.js
@@ -8,9 +8,9 @@ if ('geolocation' in navigator) {
     console.log('geolocation available');
     navigator.geolocation.getCurrentPosition(async position => {
         try {
+            console.log('Entered try')
             lat = position.coords.latitude;
             long = position.coords.longitude;
-            console.log(lat, long);
             document.getElementById('latitude').textContent = lat;
             document.getElementById('longitude').textContent = long;
             // CORS is disabled on Dark Sky, so we can't share the API's resources with client side code
@@ -19,8 +19,10 @@ if ('geolocation' in navigator) {
             const apiURL = `/weather/${lat},${long}`
             const response = await fetch(apiURL)
             const json = await response.json()
+            console.log('tried to get weather')
             const weather = json['weather']['currently']
             const aq = json['airQuality']['results'][2]
+            console.log('tried to get air qaulity')
             document.getElementById('summary').textContent = weather['summary']
             document.getElementById('temperature').textContent = weather['temperature']
             document.getElementById('aqParameter').textContent = aq['parameter']
@@ -28,7 +30,7 @@ if ('geolocation' in navigator) {
             document.getElementById('aqUnits').textContent = aq['unit']
             const aqDate = new Date(aq['date']['local'])
             document.getElementById('aqDate').textContent = aqDate.toDateString()
-            console.log(json)
+            console.log('about to try to post data')
 
 
             // Send to database 
@@ -42,6 +44,7 @@ if ('geolocation' in navigator) {
             };
             const dbResponse = await fetch('/api', options);
             const dbJSON = await dbResponse.json();
+            console.log('tried to post data')
             console.log(dbJSON);
             console.log('tried to post')
 
